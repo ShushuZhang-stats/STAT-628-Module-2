f94c1cfd7ba04e6fb6e022d3332c539e0f899701
htong25 <72429495+htong25@users.noreply.github.com>
2020-10-08 21:24:16 -0500

    Create rshiny.r

---
+++ rshiny.r
@@ -0,0 +1,39 @@
+library(shiny)
+ui <- fluidPage(
+  
+  titlePanel("Body Fat Prediction"),
+  
+  sliderInput(inputId = "weight",
+              label = "Weight(lbs):",
+              min = 100,
+              max = 300,
+              value = 200),
+  sliderInput(inputId = "neck",
+              label = "Neck circumference(cm):",
+              min = 30,
+              max = 55,
+              value = 40),
+  sliderInput(inputId = "abdomen",
+              label = "Abdomen 2 circumference(cm):",
+              min = 50,
+              max = 150,
+              value = 90),
+  sliderInput(inputId = "forearm",
+              label = "Forearm circumference(cm):",
+              min = 20,
+              max = 40,
+              value = 30),
+  sliderInput(inputId = "wrist",
+              label = "Wrist circumference(cm):",
+              min = 12,
+              max = 25,
+              value = 18),
+  verbatimTextOutput("value")
+  
+)
+server<-function(input,output){
+  output$value=renderPrint({
+    -24.42-0.12*input$weight-0.34*input$neck+0.95*input$abdomen+0.58*input$forearm-1.42*input$wrist#,input$neck,input$abdomen,input$forearm,input$wrist))
+  })
+}
+shinyApp(ui=ui,server = server)
