52f22d606386a44849a531581561f289cc0f397d
Yuan <caoy98@foxmail.com>
2020-10-12 01:38:22 -0500

    Update UI

--- rshiny.r
+++ rshiny.r
@@ -1,39 +1,57 @@
 library(shiny)
+
 ui <- fluidPage(
   
-  titlePanel("Body Fat Prediction (If you have any questions about the result, contact 608-404-4444)"),
+  titlePanel("Body Fat Prediction"),
   
-  sliderInput(inputId = "weight",
-              label = "Weight(lbs):",
-              min = 100,
-              max = 300,
-              value = 200),
-  sliderInput(inputId = "neck",
-              label = "Neck circumference(cm):",
-              min = 30,
-              max = 55,
-              value = 40),
-  sliderInput(inputId = "abdomen",
-              label = "Abdomen 2 circumference(cm):",
-              min = 50,
-              max = 150,
-              value = 90),
-  sliderInput(inputId = "forearm",
-              label = "Forearm circumference(cm):",
-              min = 20,
-              max = 40,
-              value = 30),
-  sliderInput(inputId = "wrist",
-              label = "Wrist circumference(cm):",
-              min = 12,
-              max = 25,
-              value = 18),
-  verbatimTextOutput("value")
-  
+  sidebarLayout(position = "right",
+    mainPanel(
+      textOutput("value")
+    ),
+    
+    sidebarPanel(
+      h3("Enter your information"),
+      sliderInput(inputId = "weight",
+                  label = "Weight (lbs):",
+                  min = 90,
+                  max = 400,
+                  value = 170),
+      sliderInput(inputId = "neck",
+                  label = "Neck circumference (cm):",
+                  min = 30,
+                  max = 60,
+                  value = 40),
+      sliderInput(inputId = "abdomen",
+                  label = "Abdomen 2 circumference (cm):",
+                  min = 50,
+                  max = 160,
+                  value = 90),
+      sliderInput(inputId = "forearm",
+                  label = "Forearm circumference (cm):",
+                  min = 15,
+                  max = 40,
+                  value = 30),
+      sliderInput(inputId = "wrist",
+                  label = "Wrist circumference (cm):",
+                  min = 12,
+                  max = 25,
+                  value = 18),
+      br(),
+      br(),
+      h4("Contact Us"),
+      p("If you have any questions about the result, please feel free to contact us with email."),
+      p("Hao Tong ()"),
+      p("Yuan Cao (cao234@wisc.edu)"),
+      p("Shushu Zhang ()")
+    )
+  )
 )
+
 server<-function(input,output){
-  output$value=renderPrint({
-    -24.42-0.12*input$weight-0.34*input$neck+0.95*input$abdomen+0.58*input$forearm-1.42*input$wrist#,input$neck,input$abdomen,input$forearm,input$wrist))
+  output$value=renderText({
+    paste("Your body fat is ", 
+          as.character(-24.42-0.12*input$weight-0.34*input$neck+0.95*input$abdomen+0.58*input$forearm-1.42*input$wrist), "%.", sep = "")
   })
 }
+
 shinyApp(ui=ui,server = server)
